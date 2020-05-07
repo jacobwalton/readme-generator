243c2afcd8e38de8916d87e2bef4ff47cc78a1eb
Jacob Walton <jawalton6616@gmail.com>
2020-05-07 14:36:24 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# Readme Generator
+![Languages badge](https://img.shields.io/github/languages/count/jawalton6616/readme-generator)
+## Description:
+An easy-to-use readme generator which uses Node with Util, FS, and Inquirer dependencies
+
+
+# Table of Contents:
+* [Installation ](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Questions](#questions)
+----
+
+## Installation
+Install dependencies using "npm init" and "npm i"
+
+
+## Usage
+For the use of developers looking for an easy way to make readme's for their projects.
+
+
+
+## License
+No licensing info.
+
+
+## Contributors
+Jacob Walton
+
+
+## Test
+Test by running the following command:
+"node index.js"
+
+
+
+## Questions
+
+If you have any questions you can contact me at jawalton6616@gmail.com.
--- index.js
+++ index.js
@@ -1,101 +1,110 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const util = require ('util');
-const dotenv = require ('dotenv');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const dotenv = require("dotenv");
 
-const writeFileAsync=util.promisify(fs.writeFile)
+const writeFileAsync = util.promisify(fs.writeFile);
 //ask for user inputs
 function promptUser() {
-    return inquirer.prompt([
-{
-    type:'input',
-    message: 'What the title of your project?',
-    name: 'title',
-},
-{
-    type:'input',
-    message:'BADGE INFO HERE',
-    name:'badge',
-},
-{
-    type:'input',
-    message:'Give a short description of your project',
-    name:'desc',
-},
-{
-    type:'input',
-    message:'Provide a table of contents',
-    name:'tableOfContents',
-},
-{
-    type:'input',
-    message:'How will the user install your project',
-    name:'install',
-},
-{
-    type:'input',
-    message:'Provide usage information',
-    name:'usage',
-},
-{
-    type:'input',
-    message:'Provide the licensing info for your project',
-    name:'license',
-},
-{
-    type:'input',
-    message:'How will the user run a test',
-    name:'test',
-},
-{
-    type:'input',
-    message:'QUESTIONS HEADER',
-    name:'questions',
-},
-{
-    type:'input',
-    message:'What is your GitHub email address',
-    name:'email',
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Give a short description of your project",
+      name: "desc",
+    },
+    {
+      type: "input",
+      message: "How will the user install your project",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Provide usage information",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Provide the licensing info for your project",
+      name: "license",
+    },
+    {
+    type: "input",
+    message: "Provide names of all contributors (separated by commas)",
+    name: "contributors",
+      },
+    {
+      type: "input",
+      message: "How will the user run a test",
+      name: "test",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub email address",
+      name: "email",
+    },
+  ]);
 }
-]);
-}
-function generateReadme(res){
-return `
-#${res.title}
-RESEARCH & INSERT BADGE STUFF HERE ---------------------------
-#Description:
+function generateReadme(res) {
+  return `
+# ${res.title}
+![Languages badge](https://img.shields.io/github/languages/count/${res.username}/${res.title.split(" ").join("-").toLowerCase()})
+## Description:
 ${res.desc}\n
-#Table of Contents
-INSERT LINKS FOR TOC\n
-#Installation
-Install by running the following command:
-${res.install}\n
-#Usage
 
-#License
+# Table of Contents:
+* [Installation ](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Questions](#questions)
+----
 
-#Contributors
+## Installation
+${res.install}
 
-#Test
+
+## Usage
+${res.usage}\n
+
+
+## License
+${res.license}\n
+
+
+## Contributors
+${res.contributors}\n
+
+
+## Test
 Test by running the following command:
 ${res.test}\n
-#Questions
-PROFILE PIC HERE
-If you have any questions you can contact me at ${res.email}
+
+
+## Questions
+
+If you have any questions you can contact me at ${res.email}.
 `
-
-};
-
+}
 
 promptUser()
-  .then(function(res) {
+  .then(function (res) {
     const readme = generateReadme(res);
 
     return writeFileAsync("README.md", readme);
   })
-  .then(function() {
-    console.log("Successfully generated README file");
+  .then(function () {
+    console.log("Successfully generated your README file!");
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err);
   });
